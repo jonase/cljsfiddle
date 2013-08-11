6db1fdb3501c801cfc43b8a1733db69faedb4eef
Jonas Enlund <jonas.enlund@gmail.com>
2013-08-11 08:37:42 +0300

    use heroku postgres url env variable

--- src/clj/cljsfiddle/db.clj
+++ src/clj/cljsfiddle/db.clj
@@ -4,14 +4,14 @@
             [environ.core :refer (env)]))
 
 (defn table-exists? [tablename]
-  (not (empty? (sql/with-connection (env :database-url)
+  (not (empty? (sql/with-connection (env :heroku-postgres-ivory-url)
                  (sql/with-query-results results
                    ["SELECT * FROM pg_tables WHERE tablename=?" tablename]
                    (into [] results))))))
 
 (defn create []
   (when-not (table-exists? "fiddles")
-    (sql/with-connection (env :database-url)
+    (sql/with-connection (env :heroku-postgres-ivory-url)
       (sql/create-table :fiddles
                         [:ns :varchar "PRIMARY KEY"]
                         [:cljs :text "NOT NULL"]
@@ -20,17 +20,17 @@
                         [:created :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))))
 
 (defn drop []
-  (sql/with-connection (env :database-url)
+  (sql/with-connection (env :heroku-postgres-ivory-url)
     (sql/drop-table :fiddles)))
 
 (defn upsert [fiddle]
   (let [ns (:ns fiddle)]
-    (sql/with-connection (env :database-url)
+    (sql/with-connection (env :heroku-postgres-ivory-url)
       (sql/update-or-insert-values :fiddles ["ns = ?" ns] fiddle))))
 
 (defn find-by-ns [ns]
   (first
-   (sql/with-connection (env :database-url)
+   (sql/with-connection (env :heroku-postgres-ivory-url)
      (sql/with-query-results results
        ["SELECT * FROM fiddles WHERE ns = ?" ns]
        (into [] results)))))
